498cab185a03358a2f51a93c4077ce5baff57948
Jan Bajena <bajena3@o2.pl>
2014-10-12 18:45:11 +0200

    Add automata class

---
+++ OCR/Automata.rb
@@ -0,0 +1,26 @@
+class Automata
+  def initialize(symbols_list, states_count)
+    self.symbols_list = symbols_list
+    self.states_count = states_count
+  end
+
+  def compute_symbol(symbol)
+
+  end
+
+  def compute_word(word)
+
+  end
+
+  private
+
+  attr_accessor :symbols_list, :states_count
+
+  def init_transition_lists(symbols_list, states_count)
+
+  end
+
+  def is_word_from_alphabet(word)
+
+  end
+end
