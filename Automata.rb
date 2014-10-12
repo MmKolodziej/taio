72954dca1ce1fba9b3747cb041963ecaeceb6e2a
Jan Bajena <bajena3@o2.pl>
2014-10-12 18:57:48 +0200

    Moved file

--- Automata.rb
+++ Automata.rb
@@ -1,4 +1,6 @@
 class Automata
+
+  #symbols_list: list of symbols that the automata can process (eg.: '0,1' , 'a,b,c' etc)
   def initialize(symbols_list, states_count)
     self.symbols_list = symbols_list
     self.states_count = states_count
@@ -16,9 +18,10 @@ class Automata
 
   attr_accessor :symbols_list, :states_count
 
-  def init_transition_lists(symbols_list, states_count)
+  def init_transition_lists
 
   end
+
 
   def is_word_from_alphabet(word)
 
