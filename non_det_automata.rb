ac41cbdde842bab50cc2bf61457c5e3ab64f9f60
homaralex <homaralex@gmail.com>
2014-11-17 01:42:10 +0100

    Non deterministic automata
    Non-det automata
    Non-det pso
    tests

---
+++ non_det_automata.rb
@@ -0,0 +1,125 @@
+class NdAutomata
+# deterministic automata
+# each row in a transistion matrix can contain more than one '1'
+
+#symbols_list: list of symbols that the automata can process (eg.: '0,1' , 'a,b,c' etc)
+  def initialize(symbols_list, states_count, transition_matrices=nil, no_of_possible_states = 3, rejecting_states = [])
+    if (symbols_list.uniq.count != symbols_list.count)
+      puts "Symbols: #{symbols_list} are not unique!"
+    end
+
+    self.symbols_list = symbols_list
+    self.states_count = states_count
+    self.possible_states_count = no_of_possible_states
+    self.current_states = reset_current_states
+    self.rejecting_states = rejecting_states
+
+    if transition_matrices
+      self.transition_matrices = transition_matrices
+    else
+      init_transition_matrices
+    end
+  end
+
+  # Computes a single symbol -> changes the automata's current state to the appropriate one.
+  # symbol: the symbol to be computed (eg.: '0', 'a', 'X')
+  def compute_symbol(symbol)
+    matrix = transition_matrices[symbols_list.index(symbol)]
+    states = multiply_matrix_by_vector(matrix, current_states)
+    limit_states(states)
+  end
+
+  # word: a list of symbols for the automata to compute (eg.: 'aabccaaa')
+  # returns: End state if computations successfull, False if word does not belong to automata alphabet
+  def compute_word(word)
+    return unless word_is_from_alphabet?(word)
+
+    word.each do |symbol|
+      self.current_states = compute_symbol(symbol)
+    end
+
+    end_states = current_states
+
+    # reset the current_state for future computations
+    self.current_states = reset_current_states
+
+    end_states
+  end
+
+  def word_is_from_alphabet?(word)
+    word.each do |symbol|
+      return false unless symbols_list.include?(symbol)
+    end
+
+    true
+  end
+
+  def print_transition_matrix
+    symbols_list.each do |symbol|
+      puts "Transition matrix[#{symbol}] = "
+      transition_matrices[symbols_list.index(symbol)].each { |row| puts row.inspect }
+    end
+  end
+
+  def set_transition_matrices_from_vector(vector)
+    rounded_vector = vector.map {|v| v.round}
+    matrix_index, row_index = 0, 0
+
+    while matrix_index < symbols_list.count
+      while row_index < states_count
+        vector_index = matrix_index * states_count * states_count + row_index * states_count
+        transition_matrices[matrix_index][row_index] = rounded_vector[vector_index..(vector_index + states_count - 1)]
+        row_index += 1
+      end
+
+      matrix_index += 1
+      row_index = 0
+    end
+  end
+
+  private
+
+  attr_accessor :symbols_list, :states_count, :current_states, :transition_matrices, :rejecting_states, :possible_states_count
+
+  def reset_current_states
+    states = Array.new(states_count){0}
+    states[0] = 1
+    states
+  end
+
+  def multiply_matrix_by_vector(matrix, vector)
+    # the idea is, if we can go to state x from 3 different states
+    # (assuming these are our current states),
+    # then result[x] = 3
+    result = []
+
+    matrix.each do |row|
+      val = 0
+      row.each_with_index { |cell,index | val += cell * vector[index] }
+      result << val
+    end
+    result
+  end
+
+  def limit_states(states_vector)
+    #sorted_indexes contains indexes of sorted elements of the states_vector
+    sorted_indexes = states_vector.each_with_index.sort.map(&:last)
+
+    limited_states_vector = Array.new(states_count){0}
+    # we take only the top 'possible_states_count' states
+    sorted_indexes.last(possible_states_count).each do |index|
+      limited_states_vector[index] = 1
+    end
+    limited_states_vector
+  end
+
+  # returns: Matrix n x m, where n = symbols count, m = states count, initialized with zeros.
+  def init_transition_matrices
+    self.transition_matrices = Array.new(symbols_list.count){Array.new(states_count){Array.new(states_count){0}}}
+  end
+
+  def self.generate_symbols_list(count)
+    # generates a count - length array of symbols
+    (0..count-1).map(&:to_s)
+  end
+end
--- tests/second_phase_test.rb
+++ tests/second_phase_test.rb
@@ -2,6 +2,8 @@ require 'test/unit'
 require 'FileUtils'
 require_relative '../ocr_pso'
 require_relative '../csv_image_factory'
+require_relative '../non_det_ocr_pso'
+require_relative '../non_det_automata'
 
 
 class AlienElementsTest < Test::Unit::TestCase
@@ -92,8 +94,8 @@ class AlienElementsTest < Test::Unit::TestCase
 
     # init image classes
     CsvImageFactory.instance.generate_image_templates(no_of_classes, no_of_characteristics)
-    CsvImageFactory.instance.generate_images_csv(no_of_objects, learn_set_sigma, learning_set_filepath)
-    CsvImageFactory.instance.generate_images_csv(no_of_objects, test_set_sigma, test_set_filepath)
+    CsvImageFactory.instance.generate_images_csv(no_of_objects, learn_set_sigma, learning_set_filepath, no_of_objects)
+    CsvImageFactory.instance.generate_images_csv(no_of_objects, test_set_sigma, test_set_filepath, no_of_objects)
     ################################################################
     #################################################################
     states_count += 1
@@ -110,10 +112,10 @@ class AlienElementsTest < Test::Unit::TestCase
 
     # algorithm configuration
     vel_space = Array.new(problem_size) { |i| [-3, 3] }
-    max_gens = 10000
-    pop_size = 15
+    max_gens = 1000
+    pop_size = 5
     max_vel = 3.0
-    c1, c2 = 1.0, 1.0
+    c1, c2 = 1.5, 1.0
     #####################################################################
     #####################################################################
 
@@ -124,7 +126,7 @@ class AlienElementsTest < Test::Unit::TestCase
     puts 'Transition matrix:'
 
     # test the test set
-    a = Automata.new(symbols_list, states_count)
+    a = Automata.new(symbols_list, states_count, nil, rejecting_states)
     a.set_transition_matrices_from_vector(best[:position])
 
     a.print_transition_matrix
@@ -142,5 +144,75 @@ class AlienElementsTest < Test::Unit::TestCase
     assert_in_delta(0, errors_count, images_count/2)
   end
 
+  def test_non_deterministic_ocr
+    #init the pso object
+    learning_set_filepath = 'learning_images.csv'
+    test_set_filepath = 'test_images.csv'
+    symbols_list = Automata.generate_symbols_list(4)
+    states_count = 10
+
+    #############################################################
+    ######## generate the learning set ##########################
+    # parameters
+    no_of_classes = 10
+    no_of_characteristics = 5
+    no_of_objects = 20
+
+    learn_set_sigma = 0.2
+    test_set_sigma = 0.2
+
+    non_det_val = 2
+
+    # init image classes
+    CsvImageFactory.instance.generate_image_templates(no_of_classes, no_of_characteristics)
+    CsvImageFactory.instance.generate_images_csv(no_of_objects, learn_set_sigma, learning_set_filepath)
+    CsvImageFactory.instance.generate_images_csv(no_of_objects, test_set_sigma, test_set_filepath)
+    ################################################################
+    #################################################################
+    rejecting_states = []
+    states_count += rejecting_states.count
+
+    pso = NON_DET_OCR_PSO.new(symbols_list, states_count,learning_set_filepath, non_det_val, rejecting_states)
+
+    #################################################################
+    ######## problem configuration ##################################
+    problem_size = states_count * states_count * symbols_list.count
+    search_space = Array.new(problem_size) { |i| [0.0, 1.0] }
+    images_count = pso.images_count
+    puts "we have #{no_of_classes} different symbols"
+
+    # algorithm configuration
+    max_vel = 0.1
+    vel_space = Array.new(problem_size) { |i| [-max_vel, max_vel] }
+    max_gens = 1000
+    pop_size = 5
+    c1, c2 = 2.5, 1.0
+    #####################################################################
+    #####################################################################
+
+    # execute the algorithm
+    best = pso.search(max_gens, search_space, vel_space, pop_size, max_vel, c1, c2)
+    rounded_best_vector = best[:position].map{|val| val.round }
+    puts "done! Solution: f=#{best[:cost]}"
+    puts 'Transition matrix:'
+
+    # test the test set
+    a = NdAutomata.new(symbols_list, states_count, nil, non_det_val, rejecting_states)
+    a.set_transition_matrices_from_vector(best[:position])
+
+    a.print_transition_matrix
+    test_set = OCR_PSO.create_words_from_image_vectors(CsvImageFactory.instance.load_sample_images_from_csv(test_set_filepath), symbols_list)
+
+    puts
+    puts 'Testing generated automata on test set...'
+    errors_count = 0
+    test_set.each do |image|
+      end_states = a.compute_word(image.word)
+      errors_count += 1 if not end_states[image.image_class] == 1
+    end
+    puts "Errors count #{errors_count}"
+    puts "#{(100.0 - errors_count*100.0/images_count)}% of test images computed correctly!"
+    assert_in_delta(0, errors_count, images_count/2)
+  end
 
 end
