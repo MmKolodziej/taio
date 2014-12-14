8bc235940dab7757598ecb33bb0bef54f3862b60
Jan Bajena <bajena3@o2.pl>
2014-12-14 21:01:22 +0100

    Added xlsx write library

---
+++ tests/xlsx_operations_test.rb
@@ -0,0 +1,26 @@
+require 'test/unit'
+require 'simple_xlsx_reader'
+require 'rubyXL'
+
+class XlsxOperationsTest < Test::Unit::TestCase
+  def test_simple_xlsx_reader
+    filepath = 'test_data/write_test_file.xlsx'
+    doc = SimpleXlsxReader.open(filepath)
+    sheet = doc.sheets.first
+    assert_not_nil(sheet.rows[0])
+  end
+
+  def test_ruby_xl_reader
+    filepath = 'test_data/write_test_file.xlsx'
+    File.delete(filepath) if File.exists?(filepath)
+
+    doc = RubyXL::Workbook.new
+    doc.add_worksheet('Sheet1')
+    sheet = doc.worksheets[0]
+    sheet.add_cell(0, 0, 'A1')
+    doc.write(filepath)
+    assert_equal(true, File.exists?(filepath))
+    
+    File.delete(filepath) if File.exists?(filepath)
+  end
+end
